1b3a5cae01b58600bee8a0e3119825d228bde81e
rconvery76 <rconvery76@gmail.com>
2025-02-10 19:44:08 -0500

    added all test cases

--- test/test.cpp
+++ test/test.cpp
@@ -21,3 +21,65 @@ TEST_CASE("Movie class") {
         REQUIRE_THROWS_AS(Movie("Inception", "Christopher Nolan", 148, 1, -1), std::invalid_argument);
     }
 }
+
+TEST_CASE("Create Movie")
+{
+    SECTION("valid movie creation")
+    {
+        std::vector<std::string> titles{"Movie 1", "Movie 2"};
+        std::vector<std::string> directors{"d1", "d2"};
+        std::vector<int> runtimes{137, 180};
+        std::vector<int> ids{1, 2};
+        std::vector<double> ratings{5.2, 10.0};
+        std::vector<Movie*> movies = CreateMovies(titles, directors, runtimes, ids, ratings);
+        REQUIRE(movies.size() == 2);
+        REQUIRE(movies[0]->GetTitle() == "Movie 1");
+        REQUIRE(movies[0]->GetDirector() == "d1");
+        REQUIRE(movies[0]->GetRuntime() == 137);
+        REQUIRE(movies[0]->GetRating() == 5.2);
+        REQUIRE(movies[1]->GetTitle() == "Movie 2");
+        REQUIRE(movies[1]->GetDirector() == "d2");
+        REQUIRE(movies[1]->GetRuntime() == 180);
+        REQUIRE(movies[1]->GetRating() == 10.0);
+    }
+
+    SECTION("invalid movie creation - different sizes")
+    {
+        std::vector<std::string> titles{"Movie 1", "Movie 2"};
+        std::vector<std::string> directors{"d1", "d2", "d3"};
+        std::vector<int> runtimes{137, 180};
+        std::vector<int> ids{1, 2};
+        std::vector<double> ratings{5.2, 11.0};
+        REQUIRE_THROWS_AS(CreateMovies(titles, directors, runtimes, ids, ratings), std::invalid_argument);
+    }
+
+    SECTION("invalid movie creation - negative runtime")
+    {
+        std::vector<std::string> titles{"Movie 1", "Movie 2"};
+        std::vector<std::string> directors{"d1", "d2"};
+        std::vector<int> runtimes{137, -180};
+        std::vector<int> ids{1, 2};
+        std::vector<double> ratings{5.2, 10.0};
+        REQUIRE_THROWS_AS(CreateMovies(titles, directors, runtimes, ids, ratings), std::invalid_argument);
+    }
+
+    SECTION("invalid movie creation - rating out of range [high]")
+    {
+        std::vector<std::string> titles{"Movie 1", "Movie 2"};
+        std::vector<std::string> directors{"d1", "d2"};
+        std::vector<int> runtimes{137, 180};
+        std::vector<int> ids{1, 2};
+        std::vector<double> ratings{5.2, 11.0};
+        REQUIRE_THROWS_AS(CreateMovies(titles, directors, runtimes, ids, ratings), std::invalid_argument);
+    }
+    
+    SECTION("invalid movie creation - rating out of range [low]")
+    {
+        std::vector<std::string> titles{"Movie 1", "Movie 2"};
+        std::vector<std::string> directors{"d1", "d2"};
+        std::vector<int> runtimes{137, 180};
+        std::vector<int> ids{1, 2};
+        std::vector<double> ratings{5.2, -1.0};
+        REQUIRE_THROWS_AS(CreateMovies(titles, directors, runtimes, ids, ratings), std::invalid_argument);
+    }
+}
